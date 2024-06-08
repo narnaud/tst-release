2f4259e46a2c109760f05f34e33b2f995e968dfd
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2024-06-08 10:33:34 +0200

    chore: try with doctest

--- tests/tst_spdlog.cpp
+++ tests/tst_spdlog.cpp
@@ -1,27 +1,16 @@
-#include <QTest>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 #include <spdlog/spdlog.h>
 
-///////////////////////////////////////////////////////////////////////////////
-// Tests
-///////////////////////////////////////////////////////////////////////////////
-class TestSpdlog : public QObject
+bool printSomething()
 {
-    Q_OBJECT
+    spdlog::info("Hello, {}!", "World");
+    spdlog::warn("Hello, {}!", "World");
+    spdlog::error("Hello, {}!", "World");
+    return true;
+}
 
-private slots:
-    void printSomething()
-    {
-        spdlog::info("Hello, {}!", "World");
-        spdlog::warn("Hello, {}!", "World");
-        spdlog::error("Hello, {}!", "World");
-    }
-
-    void getSomething()
-    {
-        auto logger = spdlog::get("foo");
-        QCOMPARE(logger, nullptr);
-    }
-};
-
-QTEST_MAIN(TestSpdlog)
-#include "tst_spdlog.moc"
+TEST_CASE("testing spdlog")
+{
+    CHECK(printSomething());
+}
