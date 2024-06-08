707153424c03005185a522563ff7670ce19745ed
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2024-06-08 10:47:43 +0200

    test: add Qt

--- tests/tst_spdlog.cpp
+++ tests/tst_spdlog.cpp
@@ -1,9 +1,11 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include <QApplication>
 #include <spdlog/spdlog.h>
 
 bool printSomething()
 {
+    QApplication app();
     spdlog::info("Hello, {}!", "World");
     spdlog::warn("Hello, {}!", "World");
     spdlog::error("Hello, {}!", "World");
